81f65d8ebc1db6850afa8a8af096e11315641f89
Vimal RS <vimalrs@gmail.com>
2025-12-09 23:10:10 +0530

    new branch commit

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import pandas as pd 
+import numpy as np
 
-d =[10,12,15,20]
 
-d1 =pd.Series(d,name='test',index=['a','b','c','d'])
+d1 =pd.Series(np.arange(5),name='series from array',index=['a','b','c','d','e'])
 print(d1)
+
+series_3 = pd.Series([1, 2, 3, 4, 5]) 
+print(series_3[4:5])
